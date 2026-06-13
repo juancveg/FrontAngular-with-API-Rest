40f474f3fb87efe501275cc3f1f91fcda1d3225b
Juan Carlos <juanvegarojano@hotmail.com>
2026-06-12 23:55:56 -0500

    Define application routes for posts and comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+import { CommentListComponent } from './comment-list/comment-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: PostListComponent },
+  { path: 'posts/:id/comments', component: CommentListComponent },
+  { path: '**', redirectTo: '/posts' }
+];
