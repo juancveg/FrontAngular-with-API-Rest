a3dba6ff17dd66afc029ac4166daa84d9d3c8d0c
Juan Carlos <juanvegarojano@hotmail.com>
2026-06-12 23:56:39 -0500

    Change base URL to environment variable
    Updated base URL to use environment variable for API URL.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,17 +4,22 @@ import { Observable } from 'rxjs';
 import { Post } from '../../models/post.model';
 import { User } from '../../models/user.model';
 import { Comment } from '../../models/comment.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'https://jsonplaceholder.typicode.com';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(`${this.baseUrl}/posts`);
+  }
+
+  getPostById(postId: number): Observable<Post> {
+    return this.http.get<Post>(`${this.baseUrl}/posts/${postId}`);
   }
 
   getUsers(): Observable<User[]> {
